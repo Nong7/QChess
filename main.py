52aab9c3cf9d045bf269eb14ad5006cfb575e3c5
Jaime Resano <gemailpersonal02@gmail.com>
2020-11-25 00:01:24 +0100

    Unfinished threading-multiprocessing fixes.

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.ui import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    app.setWindowIcon(QIcon("img/logo.png"))
+    window = MainWindow(app)
+    app.exec()
