ffadf8ac42d161013bceac3557075e64ff3ba7b2
AdrianRevi <71989160+AdrianRevi@users.noreply.github.com>
2020-11-30 13:54:13 +0100

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,6 @@
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QMenu, \
-                            QMenuBar
+                            QMenuBar, QAction, QApplication
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from .game_widget import GameWidget
@@ -13,6 +14,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("QChess")
         self.setMinimumSize(400, 400)
         w = QWidget()
+
         # Main layout
         self.main_layout = QVBoxLayout()
         w.setLayout(self.main_layout)
@@ -28,6 +30,29 @@ class MainWindow(QMainWindow):
 
         self.game_widget = GameWidget(self)
         self.main_layout.addWidget(self.game_widget)
+
+        # Creation of the menu bar and insertion of the different actions that it has
+        self.menu_bar = self.menuBar()
+
+        self.configuration_menu = self.menu_bar.addMenu("Configuration")
+        self.aspect_menu = self.menu_bar.addMenu("Aspect")
+        self.change_color = self.aspect_menu.addMenu("Change Color")
+
+        # Configuration action
+        self.exit_action = QAction("Exit App", self)
+        self.exit_action.setShortcut("Ctrl+Q")
+        self.exit_action.triggered.connect(lambda: QApplication.quit())
+        self.configuration_menu.addAction(self.exit_action)
+
+        self.manual = QAction("Game Manual", self)
+        self.configuration_menu.addAction(self.manual)
+
+        # Change Color Action
+        self.classic_color = QtWidgets.QAction("Classic")
+        self.change_color.addAction(self.classic_color)
+
+        self.brown_color = QtWidgets.QAction("Brown")
+        self.change_color.addAction(self.brown_color)
 
         self.show()
 
