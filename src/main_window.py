cb3520d82e33e3903451a43fd502d39d32f3b913
Jaime Resano <gemailpersonal02@gmail.com>
2020-12-04 01:05:13 +0100

    Unfinished dark mode

--- src/main_window.py
+++ src/main_window.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QMenu, \
                             QMenuBar, QAction, QApplication
-from PyQt5.QtGui import QFont, QDesktopServices
+from PyQt5.QtGui import QFont, QDesktopServices, QPalette, QColor
 from PyQt5.QtCore import Qt, QUrl, QFileInfo, QRect
 from .game_widget import GameWidget
 
@@ -27,27 +27,31 @@ class MainWindow(QMainWindow):
         # Creation of the menu bar and insertion of the different actions that it has
         self.menu_bar = self.menuBar()
 
-        self.configuration_menu = self.menu_bar.addMenu("Options")
+        # Options menu
+        self.options_menu = self.menu_bar.addMenu("Options")
+        self.action_change_theme = QAction("Set dark theme")
+        self.action_change_theme.triggered.connect(lambda: self.change_theme())
+        self.options_menu.addAction(self.action_change_theme)
 
-        # Configuration menu
+        self.manual = QAction("Game Manual", self)
+        self.options_menu.addAction(self.manual)
+
+        self.guia = QUrl.fromLocalFile(QFileInfo("./guide.pdf").absoluteFilePath())
+        self.manual.triggered.connect(lambda: QDesktopServices.openUrl(self.guia))
+
         # Restart action (restarts the game)
         self.restart_action = QAction("Restart game", self)
         # When "Restart game" is triggered it calls a lambda function which restarts the board
         self.restart_action.triggered.connect(lambda: self.game_widget.reset_board())
-        self.configuration_menu.addAction(self.restart_action)
+        self.options_menu.addAction(self.restart_action)
 
-        self.manual = QAction("Game Manual", self)
-        self.configuration_menu.addAction(self.manual)
-
-        self.guia = QUrl.fromLocalFile(QFileInfo("./guide.pdf").absoluteFilePath())
-        self.manual.triggered.connect(lambda: QDesktopServices.openUrl(self.guia))
 
         # Exit action (exits the app via clicking the action or typing the shortcut)
         self.exit_action = QAction("Exit App", self)
         self.exit_action.setShortcut("Ctrl+Q")
         # When "Exit App" is triggered it calls a lambda function that exits the app
         self.exit_action.triggered.connect(lambda: QApplication.quit())
-        self.configuration_menu.addAction(self.exit_action)
+        self.options_menu.addAction(self.exit_action)
 
         self.show()
 
@@ -77,3 +81,25 @@ class MainWindow(QMainWindow):
             self.game_widget.setGeometry(x0, y0, x, y)
 
         self.game_widget.setGeometry(x0, y0, x, y)
+
+    def change_theme(self):
+        if self.action_change_theme.text() == "Set dark theme":
+            # Set dark theme
+            dark_theme = QPalette()
+            dark_theme.setColor(QPalette.Window, QColor(53, 53, 53))
+            dark_theme.setColor(QPalette.WindowText, Qt.black)
+            dark_theme.setColor(QPalette.Base, QColor(25, 25, 25))
+            dark_theme.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+            dark_theme.setColor(QPalette.Text, Qt.white)
+            dark_theme.setColor(QPalette.Button, QColor(53, 53, 53))
+            dark_theme.setColor(QPalette.Link, QColor(200, 130, 218))
+            dark_theme.setColor(QPalette.Highlight, QColor(42, 130, 218))
+            dark_theme.setColor(QPalette.HighlightedText, Qt.black)
+            self.setPalette(dark_theme)
+            self.action_change_theme.setText("Set light theme")
+        else:
+            # Set light mode
+            light_mode = QPalette(self.style().standardPalette())
+            self.setPalette(light_mode)
+            self.action_change_theme.setText("Set dark theme")
+
